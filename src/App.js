6099c97df97e22dbcb74a1f71892312c7bbc6bc6
Alaa El-faqawi <93674732+alaaelfaqawi@users.noreply.github.com>
2025-02-27 10:45:57 -0800

    Create The File Structures For My Project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Blog from "./pages/blog/Blog";
+import Projects from "./pages/projects/Projects";
+import Contact from "./pages/contact/Contact";
+import NotFound from "./pages/notFound/NotFound";
+
 function App() {
-  return <div>Alaa</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
